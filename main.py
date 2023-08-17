baffb71ff5f1bce10fe2ce4f2a5a07f569b88153
omarelkobrossy <omarelkobrossy@yahoo.com>
2023-08-17 21:41:37 +0300

    Removed Unnecessary additions

--- main.py
+++ main.py
@@ -7,21 +7,11 @@ import threading
 
 # Create an argument parser
 parser = argparse.ArgumentParser(description="Argument parsing example")
-
-# Add mutually exclusive group for --connect and --local
-local_or_URL = parser.add_mutually_exclusive_group(required=True)
 getData_or_Monitor = parser.add_mutually_exclusive_group(required=False)
 # root_traverse_group = parser.add_mutually_exclusive_group(required=True)
 
 # Add --connect argument
-local_or_URL.add_argument("-connect", type=validate_url, help="DB Link to connect to")
-
-# Add --local argument
-local_or_URL.add_argument("-local", type=str, help="DB Local file path")
-
-# Create a mutually exclusive group for --root and --traverse
-# root_traverse_group.add_argument("root", action="store_true", help="Use root mode")
-# root_traverse_group.add_argument("traverse", action="store_true", help="Use traverse mode")
+parser.add_argument("-connect", type=validate_url, help="DB Link to connect to")
 
 #Add --chunk argument
 getData_or_Monitor.add_argument("-chunk", type=int, help="Add the chunk size")
@@ -33,7 +23,6 @@ args = parser.parse_args()
 
 # Access parsed arguments
 connect_link = args.connect
-local_path = args.local
 chunk = args.chunk
 monitor_flag = args.monitor
 flowtest = args.flowtest
